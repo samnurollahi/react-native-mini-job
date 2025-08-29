f0e7363ce5a8901d61cb6063c679e2302bad2944
sam nurollahi <samnurollahi>
2025-08-29 15:15:03 +0330

    fix bug

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import { registerRootComponent } from 'expo';
+import { I18nManager } from 'react-native';
 
 import App from './App';
+
+I18nManager.allowRTL(false)
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
